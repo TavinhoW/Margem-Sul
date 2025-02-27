5e5161e50e5d5925f83217a60ec58e90ef6140d0
TavinhoW <gustavowmatos@gmail.com>
2025-02-27 14:52:51 +0000

    CSS feito, tudo normal como antes mas sem vite

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,3 @@
-import Header from './components/Header';
-import Footer from './components/Footer';
 import Products from './frontpage/produtos';
 import Noticias from './frontpage/noticias';
 import Trofeus from './frontpage/info';
@@ -7,11 +5,9 @@ import Trofeus from './frontpage/info';
 export default function Home() {
   return (
       <>
-          <Header />
           <Products />
           <Noticias />
           <Trofeus />
-          <Footer />
       </>
   );
 }
