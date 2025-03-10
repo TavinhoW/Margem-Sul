d3e5a634f8b2710d99c107c8b5bbfed1691dd0d9
Gustavo Matos <gustavowmatos@gmail.com>
2025-03-10 12:32:32 +0000

    fix: remove profile stuff

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -13,7 +13,6 @@ const Header = () => {
                         <Link href="../content/jogos" className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white transition duration-300 ease-in-out">Jogos</Link>
                         <Link href="../content/estadio" className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white transition duration-300 ease-in-out">Estádio</Link>
                         <Link href="../content/equipa" className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white transition duration-300 ease-in-out">Equipa</Link>
-                        <Link href="../content/loja" className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white transition duration-300 ease-in-out">Loja</Link>
                     </div>
                 </div>
             </div>
